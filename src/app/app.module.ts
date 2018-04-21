573184738ba5b2db57872a36d26471aad38a5e25
Alexander Andriets <avandriets@gmail.com>
2018-04-22 01:13:16 +0300

    enhancement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 // own resources
 import {AppComponent} from './app.component';
 import {SharedModule} from './modules/shared/shared.module';
 import {CoreModule} from './core/core.module';
 import {MediaApiService} from './core/services/media-api.service';
-import {HttpClientModule} from '@angular/common/http';
 
 
 const routes: Routes = [
